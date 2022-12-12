8cbe3c0d74490a71c17c8d3bb656cf4afabc4728
ydchen17 <ydchen.lse@gmail.com>
2022-12-12 20:56:50 +0000

    update

--- sync.sh
+++ sync.sh
@@ -2,4 +2,5 @@ git add .
 git commit -m"update"
 git push
 
-rsync -aE --delete * "/Users/mbp16/Library/CloudStorage/GoogleDrive-ydchen.lse@gmail.com/My Drive/HP407/sync_files"
+rsync -aE --delete * "/Users/mbp16/Library/CloudStorage/GoogleDrive-ydchen.lse@gmail.com/My 
+Drive/HP407/project_progress"
